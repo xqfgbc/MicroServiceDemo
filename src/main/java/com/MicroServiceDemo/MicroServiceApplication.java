40a085cfa6aa7e7254486a0d17603fda5429f20b
xqfgbc <gb1932002@aliyun.com>
2017-05-03 21:24:19 +0800

    init project

---
+++ src/main/java/com/MicroServiceDemo/MicroServiceApplication.java
@@ -0,0 +1,12 @@
+package com.MicroServiceDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceApplication.class, args);
+	}
+}
